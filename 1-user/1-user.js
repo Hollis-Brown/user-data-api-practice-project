654ea8bcaf7fc3e5daa2f8ff2119e35288868d31
Hollis <hollis.brown.cohort231@gmail.com>
2024-01-29 10:43:08 -0800

    This is an edit with an added button

--- 1-user/1-user.js
+++ 1-user/1-user.js
@@ -1,39 +1,41 @@
-// Basic promise chaining using the fetch() API to perform fetching of data from a server
-
-
+// Basic promise chaining using the fetch() API to perform fetching of data from a server at the click of a button
 console.log("Hello, Good Afternoon!");
 
 const jsonStr = 'https://jsonplaceholder.typicode.com/users';
 
 let list = document.getElementById('list');
 
+// Function to fetch and display data for another user
+const loadAnotherUser = () => {
+  // Fetch data for another user
+  fetch(jsonStr)
+    .then(response => {
+      if (!response.ok) throw new Error('Invalid response');
+      return response.json();
+    })
+    .then(dataArray => {
+      // Display data for the second user (assuming there's at least one more user)
+      if (dataArray.length > 1) {
+        const secondUser = dataArray[0];
+        const userHtml = `<li class="list-group-item">
+          <p><strong>Id:</strong> ${secondUser.id}</p>
+          <p><strong>Name:</strong> ${secondUser.name}</p>
+          <p><strong>Username:</strong> ${secondUser.username}</p>
+          <p><strong>Email:</strong> ${secondUser.email}</p>
+          <p><strong>Phone:</strong> ${secondUser.phone}</p>
+          <p><strong>Website:</strong> ${secondUser.website}</p>
+        </li>`;
+        list.innerHTML = userHtml;
+      } else {
+        list.innerHTML = 'No more users available.';
+      }
+    })
+    .catch(error => {
+      console.log('Error fetching another user:', error);
+      list.innerHTML = `Could not fetch user data: ${error}`;
+    });
+};
 
-let promise1 = fetch(jsonStr)
-  
-  .then(function(response) {
-    console.log("Response received for promise1");
-    if (!response.ok) throw new Error('invalid response');
-    return response.json(); 
-  })
-  .then(function(dataArray) { 
-    console.log("Data received for promise1");
-    let firstUser = dataArray[0]; 
-    return `<li class="list-group-item"> 
-      <p><strong>Id:</strong> ${firstUser.id}</p> 
-      <p><strong>Name:</strong> ${firstUser.name}</p> 
-      <p><strong>Username:</strong> ${firstUser.username}</p> 
-      <p><strong>Email:</strong> ${firstUser.email}</p> 
-      <p><strong>Phone:</strong> ${firstUser.phone}</p> 
-      <p><strong>Website:</strong> ${firstUser.website}</p>
-    </li>`;
-  })
-  .catch(function(error) {
-    console.log("Error received for promise1");
-    console.log(error);
-    list.innerHTML = `Could not fetch user data: ${error}`;
-  });
-
-promise1.then(function(data) {
-  console.log("Data received for promise1");
-  list.innerHTML = data;
-});
+// Attach click event listener to the "Load Another User" button
+const loadUserButton = document.getElementById('loadUserButton');
+loadUserButton.addEventListener('click', loadAnotherUser);
